2b73eb7bfcf24b2dd8ec51be341ef3303b9d7613
Aelfhe1m <aelfhe1m@hotmail.com>
2017-08-01 02:46:22 +0100

    Orbital restriction

---
+++ Source/KerbalScienceInnovation/KSIGravityWaveModule.cs
@@ -0,0 +1,136 @@
+﻿using KSP.UI.Screens.Flight.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSP.Localization;
+using UnityEngine;
+
+namespace KerbalScienceInnovation
+{
+
+    //MODULE
+    //{
+        // name = KSIGravityWaveModule
+        // timeToCollect = 151200 // time between starting experiment and results available. Kerbin day is 21600 seconds
+        // fractionOfSoi = 0.9 // Pe and Ap must be > 90% of SOI radius
+        // inclination = 90 // polar orbit
+        // inclinationError = 2.5 // allow orbit inclination to be off by this number of degrees
+	
+    //}
+
+
+
+public class KSIGravityWaveModule : ModuleScienceExtended
+    {
+        [KSPField]
+        public string experimentId = "KSIGravityWaveExperiment";
+
+        [KSPField]
+        public new int resultsDelay = 151200;
+
+        [KSPField]
+        public double fractionOfSOI = 0.9;
+
+        [KSPField]
+        public double inclination = 90;
+
+        [KSPField]
+        public double allowedIncError = 2.5;
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            base.OnStart(state);
+
+            
+        }
+
+        public override bool CanRunExperiment()
+        {
+            if (!HighLogic.LoadedSceneIsFlight)
+                return false;
+
+            var msg = "";
+            var isOK = true;
+
+            var debugInfo = new StringBuilder();
+
+            debugInfo.Append("[KSI]: altitude: ");
+            debugInfo.Append(vessel.altitude);
+            debugInfo.Append(", SOI: ");
+            debugInfo.Append(vessel.mainBody.sphereOfInfluence);
+            debugInfo.Append(", Inc: ");
+            debugInfo.Append(vessel.orbit.inclination);
+
+            // Debug.Log(debugInfo.ToString());
+
+            if (vessel.orbit.PeA < fractionOfSOI * vessel.mainBody.sphereOfInfluence)
+            {
+                msg += Localizer.GetStringByTag("#KSI_Grav_PeTooLow");
+                isOK = false;
+            }
+            if (vessel.orbit.ApA < fractionOfSOI * vessel.mainBody.sphereOfInfluence)
+            {
+                msg += Localizer.GetStringByTag("#KSI_Grav_ApTooLow");
+                isOK = false;
+            }
+            if (vessel.orbit.ApA > vessel.mainBody.sphereOfInfluence)
+            {
+                msg += Localizer.GetStringByTag("#KSI_Grav_ApTooHigh");
+                isOK = false;
+            }
+
+            if (vessel.orbit.inclination > inclination + allowedIncError || vessel.orbit.inclination < inclination - allowedIncError)
+            {
+                msg += Localizer.GetStringByTag("#KSI_Grav_IncWrong");
+                isOK = false;
+            }
+
+            if (msg.Length == 0)
+                msg = Localizer.GetStringByTag("#KSI_Grav_OrbitOK");
+
+            status = msg;
+
+            return isOK;
+        }
+
+        public override void RunExperiment(bool silent = false)
+        {
+            if (!CanRunExperiment())
+            {
+                ScreenMessages.PostScreenMessage(Localizer.Format("#KSI_Grav_RequiredOrbitMessage", (fractionOfSOI * vessel.mainBody.sphereOfInfluence).ToString("N0")), 5f, ScreenMessageStyle.UPPER_CENTER);
+                return;
+            }
+
+            if (!isRunning && !isFinished)
+            {
+                ScreenMessages.PostScreenMessage(Localizer.GetStringByTag("#KSI_Grav_startedMessage"), 5f, ScreenMessageStyle.UPPER_CENTER);
+                isRunning = true;
+                startTime = Planetarium.GetUniversalTime();
+                return;
+            }
+
+            if (isFinished)
+                CreateExperimentResult();
+
+            if (!silent)
+                ReviewData();
+        }
+
+        public override void CreateExperimentResult()
+        {
+
+            storedData.Clear();
+            ScienceData data = null;
+            ScienceExperiment experiment = ResearchAndDevelopment.GetExperiment(experimentId);
+            ScienceSubject subject = ResearchAndDevelopment.GetExperimentSubject(experiment, ExperimentSituations.InSpaceHigh, vessel.mainBody, "", "");
+
+            
+
+            if (data == null)
+                return;
+            storedData.Add(data);
+        }
+
+    }
+}
---
+++ Source/KerbalScienceInnovation/ModuleScienceExtended.cs
@@ -0,0 +1,220 @@
+﻿using KSP.Localization;
+using KSP.UI.Screens.Flight.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerbalScienceInnovation
+{
+    public abstract class ModuleScienceExtended : PartModule, IScienceDataContainer
+    {
+        /// <summary>
+        /// Time between starting experiment and results being available
+        /// </summary>
+        [KSPField]
+        protected int resultsDelay = 151200;
+
+        [KSPField(isPersistant = false, guiName = "#KSI_Status", guiActive =true)]
+        public string status;
+
+        [KSPField(isPersistant = false, guiName = "#KSI_TimeRemaining", guiActive = false)]
+        public string timeRemaining;
+
+        [KSPField(isPersistant = true)]
+        public double startTime;
+
+        [KSPField]
+        public float xmitBase = 1f;
+
+        [KSPField]
+        public float xmitBonus = 0f;
+
+        [KSPField]
+        public bool isRunning;
+
+        [KSPField]
+        public bool isFinished;
+
+        public abstract void CreateExperimentResult();
+        public abstract bool CanRunExperiment();
+
+        void FixedUpdate()
+        {
+            //if (CanRunExperiment())
+            //{
+            //    Events["StartExperiment"].active = true;
+            //    Actions["StartExperiment"].active = true;
+            //}
+            //else
+            //{
+            //    Events["StartExperiment"].active = false;
+            //    Actions["StartExperiment"].active = false;
+            //}
+
+            Events["ReviewEvent"].active = storedData.Count > 0;
+            Events["EVACollect"].active = storedData.Count > 0;
+
+            Fields["timeRemaining"].guiActive = isRunning || isFinished;
+
+            UpdateTimeRemaining();
+        }
+
+        void UpdateTimeRemaining()
+        {
+            if (startTime == 0 || !isRunning)
+                return;
+
+            if (Planetarium.GetUniversalTime() > startTime + resultsDelay)
+            {
+                isFinished = true;
+                isRunning = false;
+                timeRemaining = "Complete";
+            }
+            else
+            {
+                var remaining = startTime + resultsDelay - Planetarium.GetUniversalTime();
+                timeRemaining = KSPUtil.dateTimeFormatter.PrintDateDelta(remaining, true, true, true);
+            }
+
+        }
+
+        [KSPEvent(guiActive = true, guiName = "#KSI_StartExperiment", active = true)]
+        public void StartExperiment()
+        {
+            RunExperiment();
+        }
+
+        [KSPAction("#KSI_StartExperiment")]
+        public void StartExperiment(KSPActionParam param)
+        {
+                RunExperiment();
+        }
+
+        public abstract void RunExperiment(bool silent = false);
+        
+        [KSPEvent(guiActive = true, guiName = "#KSI_Review", active = false)]
+        public void ReviewEvent()
+        {
+            ReviewData();
+        }
+
+        [KSPEvent(guiActiveUnfocused = true, guiName = "#KSI_CollectEVA", externalToEVAOnly = true, unfocusedRange = 1.5f, active = false)]
+        public void EVACollect()
+        {
+            List<ModuleScienceContainer> EVACont = FlightGlobals.ActiveVessel.FindPartModulesImplementing<ModuleScienceContainer>();
+            if (storedData.Count > 0)
+            {
+                if (EVACont.First().StoreData(new List<IScienceDataContainer>() { this }, false))
+                {
+                    foreach (ScienceData data in storedData)
+                        DumpData(data);
+                }
+            }
+        }
+
+        protected List<ScienceData> storedData = new List<ScienceData>();
+        protected ExperimentsResultDialog expDialog = null;
+
+        public override void OnLoad(ConfigNode node)
+        {
+            if (node.HasNode("ScienceData"))
+            {
+                foreach (ConfigNode storedDataNode in node.GetNodes("ScienceData"))
+                {
+                    ScienceData data = new ScienceData(storedDataNode);
+                    storedData.Add(data);
+                }
+            }
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            node.RemoveNodes("ScienceData");
+
+            foreach (ScienceData data in storedData)
+            {
+                ConfigNode storedDataNode = node.AddNode("ScienceData");
+                data.Save(storedDataNode);
+            }
+        }
+
+        #region IScienceDataContainer
+
+        public void DumpData(ScienceData data)
+        {
+            expDialog = null;
+
+            if (storedData.Contains(data))
+                storedData.Remove(data);
+        }
+
+        public ScienceData[] GetData() => storedData.ToArray();
+
+        public int GetScienceCount() => storedData.Count;
+
+        public bool IsRerunnable() => false;
+
+        public void ReturnData(ScienceData data)
+        {
+            if (data == null)
+                return;
+
+            storedData.Clear();
+
+            storedData.Add(data);
+        }
+
+        public void ReviewData()
+        {
+            if (storedData.Count < 1)
+                return;
+
+            expDialog = null;
+            ScienceData data = storedData[0];
+            expDialog = ExperimentsResultDialog.DisplayResult(
+                new ExperimentResultDialogPage(
+                    part, data, xmitBase, xmitBonus, false, "", true, new ScienceLabSearch(vessel, data), DumpData, KeepData, TransmitData, SendToLab));
+        }
+
+        private void KeepData(ScienceData data)
+        {
+            expDialog = null;
+        }
+
+        private void TransmitData(ScienceData data)
+        {
+            expDialog = null;
+
+            IScienceDataTransmitter bestTransmitter = ScienceUtil.GetBestTransmitter(vessel);
+
+            if (bestTransmitter != null)
+            {
+                bestTransmitter.TransmitData(new List<ScienceData> { data });
+                DumpData(data);
+            }
+            else if (CommNet.CommNetScenario.CommNetEnabled)
+                ScreenMessages.PostScreenMessage(Localizer.Format("#autoLOC_237738"), 3f, ScreenMessageStyle.UPPER_CENTER); // No usable, in-range Comms Devices on this vessel. Cannot Transmit Data.
+            else
+                ScreenMessages.PostScreenMessage(Localizer.Format("#autoLOC_237740"), 3f, ScreenMessageStyle.UPPER_CENTER); // No usable Comms Devices on this vessel. Cannot Transmit Data.
+        }
+
+        private void SendToLab(ScienceData data)
+        {
+            expDialog = null;
+            ScienceLabSearch labSearch = new ScienceLabSearch(vessel, data);
+
+            if (labSearch.NextLabForDataFound)
+            {
+                StartCoroutine(labSearch.NextLabForData.ProcessData(data, null));
+                DumpData(data);
+            }
+            else
+                labSearch.PostErrorToScreen();
+        }
+
+        public void ReviewDataItem(ScienceData data) => ReviewData();
+
+        #endregion
+    }
+}
