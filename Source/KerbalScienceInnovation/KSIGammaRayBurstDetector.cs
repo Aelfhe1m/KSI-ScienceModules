fa6e6fbaaff576a99eef2d3e72c56ec3eae3d0ec
Aelfhe1m <aelfhe1m@hotmail.com>
2017-09-11 00:43:12 +0100

    PartModule stub

---
+++ Source/KerbalScienceInnovation/KSIGammaRayBurstDetector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSP.Localization;
+
+namespace KerbalScienceInnovation
+{
+    public class KSIGammaRayBurstDetector : PartModule
+    {
+        public override string GetInfo()
+        {
+            return Localizer.GetStringByTag("#KSI_GRBDetector_Info");
+        }
+    }
+}
